65ae2d11775fefaea7a4c18ef418e46a73603dce
daimori <anraku90@gmail.com>
2020-02-16 12:20:56 +0900

    add useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import './App.css';
+import { ClockContext } from './context/ClockContext'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './page/Home'
 import Play from './page/Play'
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/play" component={Play} exact />
-        </Switch>
-      </Router>
-    </div>
+    <ClockContext.Provider value={600}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/play" component={Play} exact />
+          </Switch>
+        </Router>
+      </div>
+    </ClockContext.Provider>
   );
 }
 
--- src/page/Play.tsx
+++ src/page/Play.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { FC, useContext } from 'react'
 import useReactRouter from 'use-react-router'
+import { ClockContext } from '../context/ClockContext'
 
-const Play = () => {
+const Play: FC = () => {
   const { history } = useReactRouter()
+  const second = useContext(ClockContext)
   return (
     <>
       <h2>Play</h2>
-      <p>display time here</p>
+      <p>{second}</p>
       <button onClick={() => history.push('/')}>Back Home</button>
     </>
   )
