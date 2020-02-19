16eceff9ec9ffa1577514126c38228bb42c97155
daimori <anraku90@gmail.com>
2020-02-19 11:54:03 +0900

    add disable button

--- src/page/Play.tsx
+++ src/page/Play.tsx
@@ -5,32 +5,34 @@ import { ClockContext } from '../context/ClockContext'
 import styled from 'styled-components'
 import { Button } from 'semantic-ui-react'
 
-type Turn = boolean | null
+type Turn = boolean
 
 const Play: FC = () => {
+  const turnWhite: Turn = true
+  const turnBlack: Turn = false
   const { history } = useReactRouter()
   const { time } = useContext(ClockContext)
   const [whiteSecond, setWhiteSecond] = useState(time)
   const [blackSecond, setBlackSecond] = useState(time)
-  const [turnFlag, setTurnFlag] = useState<Turn>(null)
+  const [turnFlag, setTurnFlag] = useState<Turn>(turnWhite)
+  const [startFlag, setStartFlag] = useState(false)
   const [timeOver, setTimeOver] = useState(false)
-  const turnWhite: Turn = true
-  const turnBlack: Turn = false
 
-  const handleClickWhite = (e: any) => {
-    e.preventDefault()
+  const handleClickWhite = (_: any) => {
     setTurnFlag(turnBlack)
+    setStartFlag(true)
   }
-  const handleClickBlack = (e: any) => {
-    e.preventDefault()
+  const handleClickBlack = (_: any) => {
     setTurnFlag(turnWhite)
+    setStartFlag(true)
   }
 
   useEffect(() => {
     let id: any
     const delay = 100
     const delta = delay/1000
-    if (turnFlag != null) {
+    console.log(startFlag)
+    if (startFlag) {
       id = setInterval(() => {
         turnFlag ? setWhiteSecond(prev => prev - delta) :
           setBlackSecond(prev => prev - delta)
@@ -39,6 +41,7 @@ const Play: FC = () => {
 
     if (whiteSecond === 0 || blackSecond === 0) {
       setTimeOver(true)
+      setStartFlag(false)
       clearInterval(id)
     }
 
@@ -56,14 +59,12 @@ const Play: FC = () => {
             <TimerBlock>
               <h3>White</h3>
               <h3>{whiteSecond.toFixed(1)}</h3>
-              {/* <Button content="White" size="massive" onClick={handleClickWhite} disabled={turnFlag === turnBlack} /> */}
-              <WhiteButton onClick={handleClickWhite}/>
+              {turnFlag === turnWhite ? <WhiteButton onClick={handleClickWhite} /> : <DisableButton />}
             </TimerBlock>
             <TimerBlock>
               <h3>Black</h3>
               <h3>{blackSecond.toFixed(1)}</h3>
-              {/* <Button content="Black" color="black" size="massive" onClick={handleClickBlack} disabled={turnFlag == null || turnFlag === turnWhite} /> */}
-              <BlackButton onClick={handleClickBlack}/>
+              {turnFlag === turnBlack ? <BlackButton onClick={handleClickBlack} /> : <DisableButton />}
             </TimerBlock>
           </Container>
         }
@@ -92,7 +93,6 @@ const WhiteButton = styled.div`
   padding: 1rem 1rem;
   text-decoration: none;
   background: #ffffff;
-  color: #FFF;
   border: 2px solid #222222;
   border-bottom: solid 4px #444444;
   border-radius: 3px;
@@ -110,7 +110,6 @@ const BlackButton = styled.div`
   padding: 1rem 1rem;
   text-decoration: none;
   background: #555555;
-  color: #FFF;
   border-bottom: solid 4px #333333;
   border-radius: 3px;
 
@@ -118,6 +117,17 @@ const BlackButton = styled.div`
     transform: translateY(4px);
     border-bottom: solid 2px #333333;
   }
+`
+
+const DisableButton = styled.div`
+  width: 100%;
+  height: 100%;
+  display: inline-block;
+  padding: 1rem 1rem;
+  text-decoration: none;
+  background: #eeeeee;
+  border-bottom: solid 4px #bbbbbb;
+  border-radius: 3px;
 `
 
 export default Play
